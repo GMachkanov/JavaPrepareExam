673ecc817e1be43cb3c7887d99c3c1bdc8f2e6dd
Georgi Machkanov <gmachkanov@gmail.com>
2014-05-18 06:51:10 +0300

    Crossing

--- ExamPrepare/src/CrossingSequences.java
+++ ExamPrepare/src/CrossingSequences.java
@@ -5,43 +5,62 @@ import java.util.Scanner;
 public class CrossingSequences {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
+		// Read inputs
+		        int firstNum = input.nextInt();
+		        int SecondNum = input.nextInt();
+		        int ThirdNum = input.nextInt();
+		        int initialNumber = input.nextInt();
+		        int step = input.nextInt();
+		// Create list
+		        List<Integer> list = new ArrayList<Integer>();
+		        list.add(firstNum);
+		        list.add(SecondNum);
+		        list.add(ThirdNum);
 
-		int firstNum = input.nextInt();
-		int secondNum = input.nextInt();
-		int thirdNum = input.nextInt();
+		        for (int i = 3; i < list.size() + 1; i++) {
+		            int tribNum = list.get(i - 3) + list.get(i - 2) + list.get(i - 1);
+		            if (tribNum > 1000000) {
+		                break;
+		            }
+		            list.add(tribNum);
+		        }
+		        boolean foundNum = false;
+		        int nextStep = 0;
+		        if (initialNumber == list.get(0)
+		                || initialNumber == list.get(1)
+		                || initialNumber == list.get(2)) {
+		            foundNum = true;
+		        } else {
+		            while (foundNum == false) {
+		                nextStep += step;
 
-		@SuppressWarnings("unused")
-		int initialNum = input.nextInt();
-		int step = input.nextInt();
+		                // Loop for step
+		                for (int i = 0; i < 2; i++) {
+		                    initialNumber += nextStep;
 
-		int tribonaci = 0;
-		List<Integer> tibSeq = new ArrayList<Integer>();
-		tibSeq.add(firstNum);
-		tibSeq.add(secondNum);
-		tibSeq.add(thirdNum);
-		while (true) {
-
-			tribonaci = firstNum + secondNum + thirdNum;
-			firstNum = secondNum;
-			secondNum = thirdNum;
-			thirdNum = tribonaci;
-			if (tribonaci < 1000000) {
-				break;
-			} else {
-				tibSeq.add(tribonaci);
-				System.out.println(tribonaci);
-			}
-		}
-		//boolean found = false;
-//		for (int i = 0; i < step; i++) {
-//			for (int j = 0; j < tibSeq.size(); j++) {
-//				if (initialNum.equal(tibSeq[j])) {
-//
-//				}
-//			}
-//		}
-		System.out.println(tibSeq.size());
+		                    for (int j = 0; j < list.size(); j++) {
+		                        if (initialNumber == list.get(j)) {
+		                            foundNum = true;
+		                            break;
+		                        }
+		                        if (initialNumber < list.get(j)) {
+		                            break;
+		                        }
+		                    }
+		                    if (foundNum) {
+		                        break;
+		                    }
+		                }
+		                if (initialNumber > 1000000) {
+		                    break;
+		                }
+		            }
+		        }
+		        if (foundNum) {
+		            System.out.println(initialNumber);
+		        } else {
+		            System.out.println("No");
+		        }
 	}
 }
